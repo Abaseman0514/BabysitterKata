53ba585a5a4b9ca36317b9c0aa16eaeeef93c25c
Ariana Baseman <ArianaMB@yahoo.com>
2019-03-17 15:05:11 -0400

    working on pairing test

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,34 @@
-'use strict';
+var gulp        = require('gulp'),
+    jshint      = require('gulp-jshint'),
+    jasmine     = require('gulp-jasmine')
+    karma       = require('karma').Server;
 
-let gulp = require('gulp'),
-    shell = require('gulp-shell');
 
-gulp.task('server', ['node', 'karma']);
+gulp.task('default', ['watch']);
 
-gulp.task('node', shell.task('node app.js'));
-gulp.task('karma', shell.task('powershell -Command "./karma.psi"'));
+gulp.task('test', function(done){
+  new karma({
+    configFile: __dirname + '/karma.conf.js'
+  }, done).start();
+});
+
+gulp.task('jshint', function(){
+  return gulp.src('source/javascript/**/*.js')
+  .pipe(jshint())
+  .pipe(jshint.reporter('jshint-stylish'));
+});
+
+
+gulp.task('build-js', function(){
+  return gulp.src('source/javascript/**/*.js')
+    .pipe(sourcemaps.init())
+      .pipe(concat('bundle.js'))
+      .pipe(gutil.env.type === 'production' ? uglify() : gutil.noop())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('public/assets/javascript'));
+});
+
+gulp.task('watch', function(){
+  gulp.watch('spec/**/*.spec.js', ['test']);
+  gulp.watch('source/javascript/**/*.js', ['jshint', 'build-js']);
+});
--- karma.conf.js
+++ karma.conf.js
@@ -1,6 +1,86 @@
 // Karma configuration
 // Generated on Wed Mar 13 2019 21:14:34 GMT-0400 (Eastern Daylight Time)
 
+// module.exports = function(config) {
+//   config.set({
+
+//     // base path that will be used to resolve all patterns (eg. files, exclude)
+//     basePath: '',
+
+
+//     // frameworks to use
+//     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+//     frameworks: ['jasmine'],
+
+
+//     // list of files / patterns to load in the browser
+//     files: [
+//       'BabysitterKata/**/*.js',
+//       '*.js'
+//     ],
+
+
+//     // list of files / patterns to exclude
+//     exclude: [
+//     ],
+
+
+//     // preprocess matching files before serving them to the browser
+//     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+//     preprocessors: {
+//     },
+
+
+//     // test results reporter to use
+//     // possible values: 'dots', 'progress'
+//     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+//     reporters: ['progress', 'htmlDetailed'],
+
+//     plugins: [
+//       'karma-jasmine',
+//       'karma-phantomjs-launcher',
+//       'karma-html-detailed-reporter'
+//     ],
+
+//     htmlDetailed: {
+//       splitResults: false
+//     },
+
+//     // web server port
+//     port: 9876,
+
+
+//     // enable / disable colors in the output (reporters and logs)
+//     colors: true,
+
+
+//     // level of logging
+//     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+//     logLevel: config.LOG_INFO,
+
+
+//     // enable / disable watching file and executing tests whenever any file changes
+//     autoWatch: true,
+
+
+//     // start these browsers
+//     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+//     browsers: ['Chrome'],
+
+
+//     // Continuous Integration mode
+//     // if true, Karma captures browsers, runs the tests and exits
+//     singleRun: false,
+
+//     // Concurrency level
+//     // how many browser should be started simultaneous
+//     concurrency: Infinity
+//   })
+// }
+
+// Karma configuration
+// Generated on Wed Jul 22 2015 11:19:42 GMT-0400 (EDT)
+
 module.exports = function(config) {
   config.set({
 
@@ -15,13 +95,14 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'BabysitterKata/**/*.js',
-      '*.js'
+      'javascript/**/*.js',
+      'spec/**/*.spec.js'
     ],
 
 
-    // list of files / patterns to exclude
+    // list of files to exclude
     exclude: [
+      'no'
     ],
 
 
@@ -32,19 +113,10 @@ module.exports = function(config) {
 
 
     // test results reporter to use
-    // possible values: 'dots', 'progress'
+    // possible values: 'dots', 'progress', or 'mocha'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress', 'htmlDetailed'],
+    reporters: ['mocha'],
 
-    plugins: [
-      'karma-jasmine',
-      'karma-phantomjs-launcher',
-      'karma-html-detailed-reporter'
-    ],
-
-    htmlDetailed: {
-      splitResults: false
-    },
 
     // web server port
     port: 9876,
@@ -70,10 +142,6 @@ module.exports = function(config) {
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false,
-
-    // Concurrency level
-    // how many browser should be started simultaneous
-    concurrency: Infinity
+    singleRun: false
   })
 }
